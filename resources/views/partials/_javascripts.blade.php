f0e3acd58a7d8d881e2c4e8c57c9b01ba994c34d
Jay-R Magdaluyo <j.magdaluyo@seiko-it.com.ph.local>
2018-02-23 16:16:25 +0800

    Modified js and css for https

--- resources/views/partials/_javascripts.blade.php
+++ resources/views/partials/_javascripts.blade.php
@@ -1,1 +1,2 @@
-<script src="{{ asset('/js/app.js') }}"></script>
+{{--  <script src="{{ asset('/js/app.js') }}"></script>  --}}
+<script src="{{ HTML::script('js/app.js', array(), true) }}"></script>
--- resources/views/partials/_stylesheets.blade.php
+++ resources/views/partials/_stylesheets.blade.php
@@ -1,2 +1,3 @@
-<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+{{--  <link rel="stylesheet" href="{{ asset('css/app.css') }}">  --}}
+<link rel="stylesheet" href="{{ HTML::style('css/app.css', array(), true) }}">
 <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
