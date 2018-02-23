9f8e742c3a1a7b2dbd615fb3c5f6ca74e04d8392
Jay-R Magdaluyo <j.magdaluyo@seiko-it.com.ph.local>
2018-02-23 16:22:30 +0800

    Modified js and css for https

--- resources/views/partials/_stylesheets.blade.php
+++ resources/views/partials/_stylesheets.blade.php
@@ -1,2 +1,2 @@
-<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<link rel="stylesheet" href="/css/app.css">
 <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
