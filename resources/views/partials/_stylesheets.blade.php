14940023daae614cd2d1988f6b87ab0c249a0ee0
Jay-R Magdaluyo <j.magdaluyo@seiko-it.com.ph.local>
2018-02-23 17:34:13 +0800

    recommit

--- resources/views/partials/_stylesheets.blade.php
+++ resources/views/partials/_stylesheets.blade.php
@@ -1,2 +1,2 @@
-<link href="/css/app.css" rel="preload" as="style" onload="this.rel='stylesheet'"/>
+<link href="{{ asset('css/app.css') }}" rel="preload" as="style" onload="this.rel='stylesheet'"/>
 <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="preload" as="style" onload="this.rel='stylesheet'"/>
